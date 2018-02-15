a0038c65c750de30d170ded66386531bfeb4b9c7
Edward Robinson <proteenx11@gmail.com>
2018-02-15 17:19:10 +1300

    convert in to cli program

--- jsonToCsv/jsonToCsv.js
+++ jsonToCsv/jsonToCsv.js
@@ -4,19 +4,29 @@ const fetch = require('node-fetch')
 const json2csv = require('json2csv')
 const flatten = require('lodash.flatten')
 const uniq = require('lodash.uniq')
-const jsonResultToCsvRow = require('./lib/jsonResultToCsvRow')
-const url = 'https://catalogue.data.govt.nz/api/action/package_search?fq=organization:ministry-of-health'
 const fs = require('fs')
+const program = require('commander')
+const jsonResultToCsvRow = require('./lib/jsonResultToCsvRow')
 
-module.exports = {
-  convert: mapObjects
-}
+program
+  .version('0.1.0')
+  .option('-a, --agency [agency]', 'CKAN agency identifier, eg: ministry-of-health')
+  .option('-f, --file [fileName]', 'CSV filename to write to, eg: output.csv, defaults to: <agency-identifer>.csv')
+  .option('-u, --url [url]', 'ckan url, eg: https://catalogue.data.govt.nz, defaults to: https://catalogue.data.govt.nz')
+  .parse(process.argv)
 
-function mapObjects (jsonInput) {
-  const results = jsonpath.query(jsonInput, '$.result.results.*')
-  const mappedResults = results.map(jsonResultToCsvRow.map)
-  return mappedResults
+const agencyIdentifier = program.agency
+console.log(agencyIdentifier)
+
+if (!agencyIdentifier) {
+  console.error('--agency CKAN agency identifier, eg: ministry-of-health, is a required parameter. try `node jsonToCsv.js --agency ministry-of-health`')
+  process.exit()
 }
+const host = program.url || 'https://catalogue.data.govt.nz'
+const url = `${host}/api/action/package_search?fq=organization:${agencyIdentifier}`
+const fileName = program.file || agencyIdentifier + '.csv'
+
+convertOnlineReport()
 
 async function convertOnlineReport () {
   const response = await fetch(url)
@@ -25,8 +35,14 @@ async function convertOnlineReport () {
   const data = mapObjects(body)
   const fields = getHeaderFieldNames(data)
   const csv = json2csv({data, fields})
-  fs.writeFileSync('./output.csv', csv)
+  fs.writeFileSync(`./${fileName}`, csv)
   console.log('Finished, written to file.')
+}
+
+function mapObjects (jsonInput) {
+  const results = jsonpath.query(jsonInput, '$.result.results.*')
+  const mappedResults = results.map(jsonResultToCsvRow.map)
+  return mappedResults
 }
 
 function getHeaderFieldNames (data) {
@@ -36,4 +52,6 @@ function getHeaderFieldNames (data) {
   return uniqueFields
 }
 
-convertOnlineReport()
+module.exports = {
+  convert: mapObjects
+}
