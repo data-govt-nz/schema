b9b92af487d7344c8955716df1646a47c2b5121f
Edward Robinson <proteenx11@gmail.com>
2018-02-15 16:14:20 +1300

    download data from internet and write to file

--- jsonToCsv/jsonToCsv.js
+++ jsonToCsv/jsonToCsv.js
@@ -1,6 +1,9 @@
 'use strict'
 const jsonpath = require('jsonpath')
+const fetch = require('node-fetch')
 const jsonResultToCsvRow = require('./lib/jsonResultToCsvRow')
+const url = 'https://catalogue.data.govt.nz/api/action/package_search?fq=organization:ministry-of-health'
+const fs = require('fs')
 
 module.exports = {
   convert: mapObjects
@@ -11,3 +14,14 @@ function mapObjects (jsonInput) {
   const mappedResults = results.map(jsonResultToCsvRow.map)
   return mappedResults
 }
+
+async function convertOnlineReport () {
+  const response = await fetch(url)
+  const data = await response.json()
+
+  const mappedObjects = mapObjects(data)
+  const stringifiedMappedObjects = JSON.stringify(mappedObjects)
+  fs.writeFileSync('./output.csv', stringifiedMappedObjects)
+}
+
+convertOnlineReport()
